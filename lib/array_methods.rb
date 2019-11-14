c90148af3b88327e80241fae9a19336eb4c9b2ed
Benjamin Small <ben20262@aim.com>
2019-11-14 00:52:25 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,9 @@
 def find_element_index(array, value_to_find)
-  array.include?(value_to_find)
+  array.index(value_to_find)
 end
 
 def find_max_value(array)
-  array.reverse()[0]
+  array.sort.reverse()[0]
 end
 
 def find_min_value(array)
